9a9a75bc1b0d37577a030f803ce80902c4f16e22
Sergey Ryzhikov <Sergey.Ryzhikov@ihep.ru>
2016-05-29 00:02:38 +0300

    homework 1

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+""" Undacity FSWDN, Homework for MultiuserBlog project.
+"""
+import sys,os
+
+import webapp2
+import jinja2
+
+# Template engine
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(
+		loader = jinja2.FileSystemLoader(template_dir),
+		autoescape = True,
+		)
+
+def render_str(template, **params):
+	t = jinja_env.get_template(template)
+	return t.render(params) 
+
+class RequestHandler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		return render_str(template, **params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+
+# Handlers
+class Rot13Handler(RequestHandler):
+	def get(self):
+		self.render('rot13-form.html')
+	
+	def post(self):
+		utext = self.request.get('text')
+		rot13=""
+		if utext:
+			rot13 = utext.encode('rot13', errors='ignore') #ignore unicode chars
+			
+		self.render('rot13-form.html', text = rot13)
+
+app = webapp2.WSGIApplication([
+	('/', Rot13Handler)
+], debug=True)
+
+
+		
+
+
