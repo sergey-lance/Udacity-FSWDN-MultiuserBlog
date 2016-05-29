b9fdf05cf8e5b29e86e79d2897088fb1fafdca97
Sergey Ryzhikov <Sergey.Ryzhikov@ihep.ru>
2016-05-30 01:33:28 +0300

    HW signup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #
 """ Undacity FSWDN, Homework for MultiuserBlog project.
 """
-import sys,os
+import sys,os,re
 
 import webapp2
 import jinja2
@@ -44,9 +44,65 @@ class Rot13Handler(RequestHandler):
 			
 		self.render('rot13-form.html', text = rot13)
 
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+def valid_username(username):
+	return username and USER_RE.match(username)
+
+def valid_password(password):
+	return password and PASS_RE.match(password)
+
+def valid_email(email):
+	return (email=="") or EMAIL_RE.match(email)
+
+class SignupHandler(RequestHandler):
+	def get(self):
+		self.render('signup-form.html')
+	
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+		verify = self.request.get('verify')
+		email = self.request.get('email')
+		
+		params = dict(username = username,
+				email = email)
+				
+		err_params = {}
+		
+		if not valid_email(email):
+			err_params['err_email'] = 'Email is not valid.'
+		
+		if not valid_username(username):
+			err_params['err_username'] = 'Username is not valid.'
+		
+		if not valid_password(password):
+			err_params['err_password'] = 'Password is not valid.'
+			
+		if password != verify:
+			err_params['err_verify'] = 'Passwords doesn\'t match.'
+		
+		if err_params:
+			self.render('signup-form.html',  **dict(params, **err_params) 	)
+		else:
+			self.redirect('/welcome?username=%s' % username)
+		
+class WelcomeHandler(RequestHandler):
+	def get(self):
+		username = self.request.get('username')
+		if valid_username(username):
+			self.render('welcome.html', username = username)
+		else:
+			self.redirect('/signup')
+
 app = webapp2.WSGIApplication([
-	('/', Rot13Handler),
+	('/', SignupHandler),
+	('/signup', SignupHandler),
 	('/rot13', Rot13Handler),
+	('/welcome', WelcomeHandler),
 ], debug=True)
 
 
